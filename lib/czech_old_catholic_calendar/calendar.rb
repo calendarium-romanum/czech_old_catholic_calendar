8ff260dc47b300c56f082f1f6296fdf871eba075
Jakub Pavlik <severus@post.cz>
2020-12-30 23:20:45 +0100

    ouch, our custom Temporale was not being used

--- lib/czech_old_catholic_calendar/calendar.rb
+++ lib/czech_old_catholic_calendar/calendar.rb
@@ -1,5 +1,18 @@
 module CzechOldCatholicCalendar
   class Calendar < CalendariumRomanum::Calendar
+    def initialize(year, sanctorale = nil, temporale = nil, vespers: false)
+      super(year, sanctorale, temporale, vespers: vespers)
+
+      # use our custom Temporale
+      #
+      # TODO: it would be better to allow changing default Temporale class
+      #   in a declarative fashion, without overriding the constructor like this
+      #   (and then having to re-generate transfers)
+      @temporale = temporale || Temporale.new(year)
+
+      @transferred = CalendariumRomanum::Transfers.call(@temporale, @sanctorale).freeze
+    end
+
     def day(*args, vespers: false)
       r = super(*args, vespers: vespers)
       r.celebrations.delete_if {|c| c.symbol == :saturday_memorial_bvm }
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -6,14 +6,27 @@ RSpec.describe COCC::Calendar do
   let(:sanctorale) { COCC::Data::GENERAL.load }
   let(:calendar) { described_class.new year, sanctorale }
 
+  it 'uses our custom Temporale by default' do
+    expect(calendar.temporale).to be_a COCC::Temporale
+  end
+
   describe 'notable differences from CalendariumRomanum::Calendar' do
-    it 'does not have Saturday memorial of BVM' do
-      saturdays = calendar.each.select {|day| day.date.saturday? }
+    describe '"temporale" celebrations not inherited' do
+      it 'Saturday memorial of BVM' do
+        saturdays = calendar.each.select {|day| day.date.saturday? }
 
-      expect(saturdays).not_to be_empty # make sure
+        expect(saturdays).not_to be_empty # make sure
 
-      expect(saturdays.find {|day| day.celebrations.collect(&:symbol).include? :saturday_memorial_bvm })
-        .to be nil
+        expect(saturdays.find {|day| day.celebrations.collect(&:symbol).include? :saturday_memorial_bvm })
+          .to be nil
+      end
+
+      it 'BVM, Mother of the Church' do
+        day = calendar[Date.new(2018, 5, 21)]
+
+        expect(day.celebrations.size).to be 1
+        expect(day.celebrations.first).to be_ferial
+      end
     end
 
     # TODO: move to Temporale specs
