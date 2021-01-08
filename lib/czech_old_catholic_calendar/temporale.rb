4676c675ebafa14b27873d32d47295654be09f19
Jakub Pavlik <severus@post.cz>
2021-01-08 11:22:11 +0100

    Temporale: inherit from CR::BaseTemporale

--- lib/czech_old_catholic_calendar/temporale.rb
+++ lib/czech_old_catholic_calendar/temporale.rb
@@ -1,57 +1,42 @@
 module CzechOldCatholicCalendar
-  class Temporale < CalendariumRomanum::Temporale
-    class << self
-      # TODO this is basically copy-pasted piece of CalendariumRomanum::Temporale.
-      #   CalendariumRomanum should really provide a better way to override the list of
-      #   movable feasts.
-      #
-      # @api private
-      def celebrations
-        @celebrations ||=
-          begin
-            [
-              :nativity,
-              :holy_family,
-              :mother_of_god,
-              :epiphany,
-              :baptism_of_lord,
-              :ash_wednesday,
-              :good_friday,
-              :holy_saturday,
-              :palm_sunday,
-              :easter_sunday,
-              :ascension,
-              :pentecost,
-              :holy_trinity,
-              :corpus_christi,
-              # :mother_of_church,
-              # :sacred_heart,
-              # :christ_king,
-              # :immaculate_heart,
-            ].collect do |symbol|
-              date_method = symbol
-              C.new(
-                date_method,
-                CalendariumRomanum::Temporale::CelebrationFactory.public_send(symbol)
-              )
-            end
-          end
-      end
+  class Temporale < CalendariumRomanum::BaseTemporale
+
+    set_seasons CalendariumRomanum::Seasons.all
+    celebration_dates CalendariumRomanum::Temporale::Dates
+
+    celebration :nativity
+    celebration :holy_family
+    celebration :mother_of_god
+    celebration :epiphany
+    celebration :baptism_of_lord
+    celebration :ash_wednesday
+    celebration :good_friday
+    celebration :holy_saturday
+    celebration :palm_sunday
+    celebration :easter_sunday
+    celebration :ascension
+    celebration :pentecost
+    celebration :holy_trinity
+    celebration :corpus_christi
+
+    # @return [Date]
+    def first_advent_sunday
+      CalendariumRomanum::Temporale::Dates.public_send __method__, year
     end
 
     private
 
     # overridden
-    def sunday(date)
-      r = super(date)
+    def sunday(date, season_week)
+      r = super(date, season_week)
 
       return r if r.nil?
 
       # TODO we duplicate logic from the parent class. Since special Sunday names
       #   are used in the Roman Catholic church, too, support for this should be moved
       #   upstream to calendarium-romanum
-      season = season(date)
-      week = season_week(season, date)
+      season = season_week.season
+      week = season_week.week
       if season == CzechOldCatholicCalendar::Seasons::ORDINARY && [2, 34].include?(week)
         return r.change(title: r.title + ' - ' + I18n.t("temporale.ordinary.sunday_name.#{week}"))
       end
@@ -59,8 +44,8 @@ module CzechOldCatholicCalendar
       r
     end
 
-    def ferial(date)
-      r = super(date)
+    def ferial(date, season_week)
+      r = super(date, season_week)
 
       if r.rank == CzechOldCatholicCalendar::Ranks::FERIAL_PRIVILEGED
         return r.change(rank: CzechOldCatholicCalendar::Ranks::FERIAL)
