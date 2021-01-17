a3291bfc5ec6ee6abeaef78b2af95f5f877f8446
Jakub Pavlik <severus@post.cz>
2021-01-17 17:24:02 +0100

    update

--- spec/calendarium_romanum_stuff_spec.rb
+++ spec/calendarium_romanum_stuff_spec.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 RSpec.describe 'stuff from CalendariumRomanum' do
   describe 'inherited unchanged' do
     it 'main classes' do
-      expect(COCC::Temporale).to be < CR::Temporale
+      expect(COCC::Temporale).to be < CR::BaseTemporale
       expect(COCC::Sanctorale).to be CR::Sanctorale
       expect(COCC::Calendar).to be < CR::Calendar
     end
@@ -17,7 +17,7 @@ RSpec.describe 'stuff from CalendariumRomanum' do
 
   describe 'not inherited' do
     it 'temporale extensions' do
-      expect(COCC::Temporale::Extensions).not_to be_const_defined 'Extensions'
+      expect(COCC::Temporale).not_to be_const_defined 'Extensions'
     end
   end
 end
